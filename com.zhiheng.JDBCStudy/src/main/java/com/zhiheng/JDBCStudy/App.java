5653cb4314cb2b5ba5aea2fe3bad2c8664f61606
yunikoro <542031180@qq.com>
2017-04-11 23:38:49 +0800

    connection builder

--- com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/App.java
+++ com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/App.java
@@ -1,13 +1,19 @@
 package com.zhiheng.JDBCStudy;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        ConnectionBuilder connBuilder = new ConnectionBuilder();
+    	try {
+        	Connection conn = connBuilder.getConnection();
+        } catch (SQLException e) {
+        	System.out.println("SQLState: "+e.getSQLState());
+        }
     }
 }
---
+++ com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/ConnectionBuilder.java
@@ -0,0 +1,25 @@
+package com.zhiheng.JDBCStudy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionBuilder {
+	private Connection conn;
+	private Properties loginProperties;
+	private String url;
+	
+	public Connection getConnection() throws SQLException{
+		this.conn = DriverManager.getConnection(this.url, this.loginProperties);
+		return conn;
+	}
+	
+	public ConnectionBuilder() {
+		this.conn = null;
+		this.loginProperties = new Properties();
+		this.loginProperties.setProperty("user", "root");
+		this.loginProperties.setProperty("password", "ilikecrow451454");
+		this.url = "jdbc:mysql://localhost:3306/coffee";
+	}
+}
