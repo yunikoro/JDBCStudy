60bbcadbb9ec12322cab2226c8553af8bcad610e
cornelius huang <cornelius.huang@capgemini.com>
2017-04-12 17:34:28 +0800

    driver builder

---
+++ com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/connection/DriverConnectionBuilder.java
@@ -0,0 +1,25 @@
+package com.zhiheng.JDBCStudy.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DriverConnectionBuilder {
+	private Connection conn;
+	private Properties loginProperties;
+	private String url;
+	
+	public Connection getConnection() throws SQLException{
+		this.conn = DriverManager.getConnection(this.url, this.loginProperties);
+		return conn;
+	}
+	
+	public DriverConnectionBuilder() {
+		this.conn = null;
+		this.loginProperties = new Properties();
+		this.loginProperties.setProperty("user", "root");
+		this.loginProperties.setProperty("password", "ilikecrow451454");
+		this.url = "jdbc:mysql://localhost:3306/coffee";
+	}
+}
