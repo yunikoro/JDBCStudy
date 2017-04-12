4054ec3e9356e46c7cd95b3a5fb352c5943b28be
yunikoro <542031180@qq.com>
2017-04-12 23:32:22 +0800

    datasource

--- com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/ConnectionBuilder.java
+++ com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/ConnectionBuilder.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import javax.sql.DataSource;
 
 public class ConnectionBuilder {
 	private Connection conn;
