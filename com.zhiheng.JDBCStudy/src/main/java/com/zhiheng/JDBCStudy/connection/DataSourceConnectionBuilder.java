a4befe2cca7e118209bbb8fa938aac56a5184533
cornelius huang <cornelius.huang@capgemini.com>
2017-04-14 18:25:35 +0800

    context

--- com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/connection/DataSourceConnectionBuilder.java
+++ com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/connection/DataSourceConnectionBuilder.java
@@ -1,5 +1,9 @@
 package com.zhiheng.JDBCStudy.connection;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class DataSourceConnectionBuilder {
@@ -8,4 +12,26 @@ public class DataSourceConnectionBuilder {
 		
 	}
 
+	public static void main(String[] args) {
+		MysqlDataSource dataSource = new MysqlDataSource();
+		dataSource.setURL("jdbc:mysql://localhost:3306/coffee");
+		try {
+			Context ctx = new InitialContext();
+			ctx.bind("jdbc/coffee", dataSource);
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		try {
+			Context ctx = new InitialContext();
+			MysqlDataSource mydataSource = (MysqlDataSource)ctx.lookup("jdbc/coffee");
+			Connection conn = mydataSource.getConnection("root", "ilikecrow451454");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 }
