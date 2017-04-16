b36264228e34510c9e003ab19b5baa041935fb6c
yunikoro <542031180@qq.com>
2017-04-16 23:03:39 +0800

    http://docs.oracle.com/javase/tutorial/jdbc/basics/retrieving.html#rs_interface       Inserting Rows in ResultSet Objects

--- com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/App.java
+++ com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/App.java
@@ -1,7 +1,18 @@
 package com.zhiheng.JDBCStudy;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.zhiheng.JDBCStudy.connection.DataSourceConnectionBuilder;
 
 /**
  * Hello world!
@@ -12,8 +23,31 @@ public class App {
         ConnectionBuilder connBuilder = new ConnectionBuilder();
     	try {
         	Connection conn = connBuilder.getConnection();
+        	Statement stmt = null;
+        	String query = "select cof_name, sup_id, price, sales, total from coffee.coffees";
+        	stmt = conn.createStatement();
+        	ResultSet rs = stmt.executeQuery(query);
+        	ResultSetMetaData rsmd = rs.getMetaData();
+        	Integer colCount = rsmd.getColumnCount();
+        	System.out.println(colCount);
+        	for(int i = 1; i <= colCount; i++) {
+        		String typeName = rsmd.getColumnTypeName(i);
+        		String colName = rsmd.getColumnName(i);
+        		System.out.println(colName + ", " + typeName);
+        	}
+        	System.out.println(rs.TYPE_FORWARD_ONLY);
+        	while(rs.next()) {
+        		Integer coffeeName = rs.getInt("cof_name");
+        		String price = rs.getString("price");
+        		System.out.println(coffeeName +", "+ price);
+        	}
         } catch (SQLException e) {
-        	System.out.println("SQLState: "+e.getSQLState());
+        	System.out.println("SQLState: " + e.getSQLState());
+        	System.out.println("Error Code: "+ e.getErrorCode());
+        	System.out.println("Message: " + e.getMessage());
+        } finally {
+        	
         }
+    	
     }
 }
--- com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/connection/DataSourceConnectionBuilder.java
+++ com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/connection/DataSourceConnectionBuilder.java
@@ -1,11 +1,18 @@
 package com.zhiheng.JDBCStudy.connection;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class DataSourceConnectionBuilder {
 
+	
+	
 	public DataSourceConnectionBuilder() {
+		MysqlDataSource mds = new MysqlDataSource();
+		mds.setURL("jdbc:mysql://localhost:3306/coffee");
 		
 	}
-
 }
