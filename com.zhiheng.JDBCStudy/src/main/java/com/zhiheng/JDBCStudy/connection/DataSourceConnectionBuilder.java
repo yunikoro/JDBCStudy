7a31352775fb3c4a0f4a3ef5abad14ceea161326
cornelius huang <cornelius.huang@capgemini.com>
2017-04-18 16:29:04 +0800

    InitialContextFactory  http://docs.oracle.com/javase/jndi/tutorial/provider/basics/initial.html and dataSource commit

--- com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/connection/DataSourceConnectionBuilder.java
+++ com.zhiheng.JDBCStudy/src/main/java/com/zhiheng/JDBCStudy/connection/DataSourceConnectionBuilder.java
@@ -1,5 +1,8 @@
 package com.zhiheng.JDBCStudy.connection;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -7,12 +10,21 @@ import javax.naming.NamingException;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class DataSourceConnectionBuilder {
-
 	
+	private MysqlDataSource mds;
+	
+	public Connection getConnection() throws SQLException {
+		return this.mds.getConnection();
+	}
+	
+	public MysqlDataSource getMySqlDataSource() {
+		return this.mds;
+	}
 	
 	public DataSourceConnectionBuilder() {
-		MysqlDataSource mds = new MysqlDataSource();
+		mds = new MysqlDataSource();
 		mds.setURL("jdbc:mysql://localhost:3306/coffee");
-		
+		mds.setUser("root");
+		mds.setPassword("ilikecrow451454");
 	}
 }
