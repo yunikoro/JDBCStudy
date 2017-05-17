99110811b36901213a9fcd6bcaa7745b98aab9b6
cornelius huang <cornelius.huang@capgemini.com>
2017-05-17 16:10:49 +0800

    https://dev.mysql.com/doc/refman/5.7/en/stored-routines.html      Mysql      Using Stored Routines (Procedures and Functions)

--- menagerie.sql
+++ menagerie.sql
@@ -222,6 +222,24 @@ UNLOCK TABLES;
 --
 -- Dumping routines for database 'menagerie'
 --
+/*!50003 DROP FUNCTION IF EXISTS `hello` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` FUNCTION `hello`(s char(20)) RETURNS char(50) CHARSET utf8
+    DETERMINISTIC
+return concat('Hello, ', s, '!') ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -232,4 +250,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-16 18:22:56
+-- Dump completed on 2017-05-17 16:08:54
