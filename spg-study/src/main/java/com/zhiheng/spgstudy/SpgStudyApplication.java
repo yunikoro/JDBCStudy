0e0b0144f616d789cff125b08c17c6ccd315986d
cornelius huang <cornelius.huang@capgemini.com>
2017-05-31 15:18:10 +0800

    http://docs.spring.io/spring-boot/docs/1.5.3.RELEASE/reference/htmlsingle/      spring      11.1 Creating the POM

---
+++ spg-study/src/main/java/com/zhiheng/spgstudy/SpgStudyApplication.java
@@ -0,0 +1,12 @@
+package com.zhiheng.spgstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpgStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpgStudyApplication.class, args);
+	}
+}
