c6c158113bc0ec10c4a784868a3cf1a1969b56b2
yunikoro <542031180@qq.com>
2017-06-11 13:25:52 +0800

    http://www.mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/   mybatis-boot-start doc      base configure finish

--- spg-study/src/main/java/com/zhiheng/spgstudy/dao/CoffeeDao.java
+++ spg-study/src/main/java/com/zhiheng/spgstudy/dao/CoffeeDao.java
@@ -3,11 +3,13 @@ package com.zhiheng.spgstudy.dao;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Component;
 
 import com.zhiheng.spgstudy.domain.Coffees;
 
+@Component
 public class CoffeeDao extends BaseDao {
-
+	
 	public CoffeeDao(SqlSession sqlSession) {
 		super(sqlSession);
 	}
--- spg-study/src/main/java/com/zhiheng/spgstudy/domain/Coffees.java
+++ spg-study/src/main/java/com/zhiheng/spgstudy/domain/Coffees.java
@@ -1,5 +1,36 @@
 package com.zhiheng.spgstudy.domain;
 
 public class Coffees {
-
+	private String cofName;
+	private Double price;
+	private Integer sales;
+	private Integer totals;
+	public String getCofName() {
+		return cofName;
+	}
+	public void setCofName(String cofName) {
+		this.cofName = cofName;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Integer getSales() {
+		return sales;
+	}
+	public void setSales(Integer sales) {
+		this.sales = sales;
+	}
+	public Integer getTotals() {
+		return totals;
+	}
+	public void setTotals(Integer totals) {
+		this.totals = totals;
+	}
+	
+	public Coffees() {
+		super();
+	}
 }
